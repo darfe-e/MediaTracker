fc0a856a43bc0450947e88305c9ecc0a252f17a5
darfe-e <belskaya.anastasiya@gmail.com>
2026-03-30 13:06:56 +0300

    added bulk-operation and tests

--- src/test/java/org/example/mediatracker/MediaTrackerApplicationTests.java
+++ src/test/java/org/example/mediatracker/MediaTrackerApplicationTests.java
@@ -1,12 +1,14 @@
 package org.example.mediatracker;
 
 import org.example.animetracker.AnimeTrackerApplication;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 
 @SpringBootTest(classes = AnimeTrackerApplication.class)
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class MediaTrackerApplicationTests {
 
   @Test
