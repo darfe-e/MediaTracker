49bd9cf4bb99584fac19ee48f3906b2e3853f6f5
darfe-e <belskaya.anastasiya@gmail.com>
2026-03-30 13:04:09 +0300

    added bulk-operation and tests

--- src/test/java/org/example/mediatracker/MediaTrackerApplicationTests.java
+++ src/test/java/org/example/mediatracker/MediaTrackerApplicationTests.java
@@ -1,16 +1,16 @@
 package org.example.mediatracker;
 
+import org.example.animetracker.AnimeTrackerApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 
-@SpringBootTest
+@SpringBootTest(classes = AnimeTrackerApplication.class)
 @ActiveProfiles("test")
 class MediaTrackerApplicationTests {
 
   @Test
   void contextLoads() {
     // This test passes if the Spring application context loads successfully.
-    // No additional assertions are needed.
   }
 }
