86341ba92360dc35275f266fca03d29dbc28995b
darfe-e <belskaya.anastasiya@gmail.com>
2026-04-23 00:53:12 +0300

    some changes for right sort of episodes

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://mediatracker-84o.pages.dev',
   withCredentials: false,
 });
 const J = { 'Content-Type': 'application/json' };
